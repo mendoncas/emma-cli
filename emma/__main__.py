0c6ffec0aa088b93a7be9a60c282392684ee2116
Ricardo <ricardo@pop-os.localdomain>
2021-11-02 21:27:27 -0300

    basic download functionality

--- emma/__main__.py
+++ emma/__main__.py
@@ -1,13 +1,21 @@
 import click
 import requests
+import os
+from pytube import YouTube
 from . import page_content as pc
 
 
 @click.command()
 @click.argument("video_url")
 def main(video_url):
-    print(pc.video_data(requests.get(video_url).text))
+    timestamps = pc.video_data(requests.get(video_url).text)
+    yt = YouTube(video_url)
+    yt.streams.filter(only_audio=True).first().download(
+        output_path='./'+yt.title)
+    os.rename('./'+yt.title+'/'+yt.title+'.mp4',
+              './'+yt.title+'/'+yt.title+'.mp3')
 
+    print('success i guess')
 
 if __name__ == '__main__':
     main()
